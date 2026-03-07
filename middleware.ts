1fbb6e12959fef7a5bc0fbd3d5c03347cb0bebd5
MeetRamatri <meetramatri12@gmail.com>
2026-03-07 16:32:58 +0530

    fix clerk middleware handshake error

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/chat(.*)", "/users(.*)"]);
+export const runtime = "experimental-edge";
 
-export default clerkMiddleware((auth, req) => {
-    // Let Clerk safely process internal handshakes without edge exceptions
-    if (req.nextUrl.searchParams.has('__clerk_handshake')) {
-        return;
-    }
 
+export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) {
-        const { userId } = auth();
-        if (!userId) {
-            return auth().redirectToSignIn();
-        }
+        await auth().protect();
     }
 });
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+    matcher: ["/((?!_next|.*\\..*).*)"],
 };
